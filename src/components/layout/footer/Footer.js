81192ed052ac91f098370c7723cb7b62e0a65bac
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-10-05 23:01:31 +0300

    Footer fixed and social buttons add

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,32 +1,41 @@
 import React from 'react'
 import Navigation from '../Navigation';
 
+
 const Footer = () => {
     return (
         <div className="footer-container">
             <div className="footer">
-                    <div className="rows">
+                
+                <div className="rows">
 
-                        <div className="colum">
-                            <ul className="footer-list">
-                                <li>צור קשר</li>
-                                <li>מוקד אבידות ומציאות</li>
-                                <li>מידע למאמצים</li>
-                                <li>מידע למוסרים בע״ח לאימוץ</li>
-                            </ul>
-                        </div>
-
-                        <div className="colum">
-                            <ul className="footer-list">
-                                <li>נהלי האימוץ</li>
-                                <li>עיקור וסירוס חתולי רחוב</li>
-                                <li>תרומות</li>
-                                <li>התנדבות</li>
-                            </ul>
-                        </div>
-
+                    <div className="colum">
+                        <ul className="footer-list">
+                            <li>צור קשר</li>
+                            <li>מוקד אבידות ומציאות</li>
+                            <li>מידע למאמצים</li>
+                            <li>מידע למוסרים בע״ח לאימוץ</li>
+                        </ul>
                     </div>
+
+                    <div className="colum">
+                        <ul className="footer-list">
+                            <li>נהלי האימוץ</li>
+                            <li>עיקור וסירוס חתולי רחוב</li>
+                            <li>תרומות</li>
+                            <li>התנדבות</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="socialPlaformButtons">
+                    <img className="socialButton" src='/images/twitter.png' alt="Twitter" />
+                    <img className="socialButton" src='/images/instagram.png' alt="Instagram" />
+                    <img className="socialButton" src='/images/facebook.png' alt="Facebook" />
+                </div>
+
             </div>
+
         </div>
     )
 }
