8ed522e6a612f6f55351cf416b422fa5ced34806
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-10-05 08:30:40 +0300

    seperated list and footer background

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -3,28 +3,30 @@ import Navigation from '../Navigation';
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="container">
-                <div className="rows">
+        <div className="background-footer">
+            <div className="footer">
+                <div className="container">
+                    <div className="rows">
 
-                    <div className="colum">
-                        <ul className="footer-list">
-                            <li>צור קשר</li>
-                            <li>מוקד אבידות ומציאות</li>
-                            <li>מידע למאמצים</li>
-                            <li>מידע למוסרים בע״ח לאימוץ</li>
-                        </ul>
+                        <div className="colum">
+                            <ul className="footer-list">
+                                <li>צור קשר</li>
+                                <li>מוקד אבידות ומציאות</li>
+                                <li>מידע למאמצים</li>
+                                <li>מידע למוסרים בע״ח לאימוץ</li>
+                            </ul>
+                        </div>
+
+                        <div className="colum">
+                            <ul className="footer-list" >
+                                <li>נהלי האימוץ</li>
+                                <li>עיקור וסירוס חתולי רחוב</li>
+                                <li>תרומות</li>
+                                <li>התנדבות</li>
+                            </ul>
+                        </div>
+
                     </div>
-
-                    <div className="colum">
-                        <ul className="footer-list" >
-                            <li>נהלי האימוץ</li>
-                            <li>עיקור וסירוס חתולי רחוב</li>
-                            <li>תרומות</li>
-                            <li>התנדבות</li>
-                        </ul>
-                    </div>
-
                 </div>
             </div>
         </div>
