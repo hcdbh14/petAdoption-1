3e8cc1f600608adc06cb32d818e8949e3e8f908c
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-10-09 18:46:42 +0300

    Started work on title - new branch after pull

---
+++ src/components/layout/homePage/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <div className="intro-container">
+            <div className="title-container">
+                <b className="intro-title-begin">אמצו אותי גם אני</b>
+                <b className="intro-title-end">רוצה בית</b>
+            </div>
+        
+        </div>
+    )
+}
+
+export default Intro
--- src/components/screens/HomePage.js
+++ src/components/screens/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Intro from '../layout/homePage/Intro';
 import SearchSection from '../layout/homePage/SearchSection';
 import PetsToAdoptSection from '../layout/homePage/PetsToAdoptSection';
 import AboutSection from '../layout/homePage/AboutSection';
@@ -6,8 +7,9 @@ const HomePage = () => {
 
     return (
         <div>
-            <h1>Homepage</h1>
-            <SearchSection />
+            {/* <h1>Homepage</h1> */}
+            {/* <SearchSection /> */}
+            <Intro />
             <PetsToAdoptSection />
             <AboutSection />
         </div>
