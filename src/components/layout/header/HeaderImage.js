3c64713efe4effee814a4df27b67076bfde3f80b
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-10-23 20:30:06 +0300

    work in progress

--- src/components/layout/header/HeaderImage.js
+++ src/components/layout/header/HeaderImage.js
@@ -3,40 +3,25 @@ import React, { useState } from 'react';
 import { switchSearchCategory } from '../../../store/headerSearch/action';
 import { useSelector, useDispatch } from 'react-redux';
 
-
-
-
-
-
-// const dogSearch = (
-//     <div>
-//             <input type="text" name="myText" value="Norway"
-//                     selectBoxOptions="Canada;Denmark;Finland;Germany;Mexico"/> 
-//     </div>
-// )
-
 const HeaderImage = () => {
 
     const searchState = useSelector(state => state.headerSearchReducer);
     const dispatch = useDispatch();
 
     const handleCategorySwitch = (userChoice) => {
-    dispatch(switchSearchCategory(1));
-    }       
+        dispatch(switchSearchCategory(userChoice));
+    }
 
     return (
         <div className="header__mainImage">
-            {/* <img src="../../../images/headerImage.png" alt="pet with owner" className="header__image" /> */}
             <input type="image" className="login-button" src={require('../../../images/login-icon.png')} alt="login-button" />
             <div className="search-container">
-            {searchState.searchChoice == 0 ? <h3>true</h3> : <h3>false</h3>}
-            <input type="image" className="search-button" src={require('../../../images/search-icon.png')} alt="search-button" />
-            <button type="button" className="search-category-button" onClick={handleCategorySwitch}>כלבים</button>
-            <button type="button" className="search-category-button" onClick={handleCategorySwitch}>חתולים</button>
-            <button type="button" className="search-category-button">חיות אחרות</button>
-            <button type="button" className="search-category-button">עמותות אימוץ</button>
-            {/* <input type="text" name="myText" value="Norway"
-                    selectBoxOptions="Canada;Denmark;Finland;Germany;Mexico">  */}
+                {searchState.searchChoice == 0 ? <h3>true</h3> : <h3>false</h3>}
+                <input type="image" className="search-button" src={require('../../../images/search-icon.png')} alt="search-button" />
+                <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(0)}>כלבים</button>
+                <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(1)}>חתולים</button>
+                <button type="button" className="search-category-button">חיות אחרות</button>
+                <button type="button" className="search-category-button">עמותות אימוץ</button>
             </div>
         </div>
     )
--- src/store/headerSearch/action.js
+++ src/store/headerSearch/action.js
@@ -1,17 +1,17 @@
 import { searchTypes } from './searchTypes';
 
-    export const switchSearchCategory = (searchChoice) => {
+export const switchSearchCategory = (searchChoice) => {
 
-        switch(searchChoice) {
-            case 0:
-                return { type: searchTypes.DOGS }
-            case 1:
-                return { type: searchTypes.CATS }
-            case 2:
-                return { type: searchTypes.OTHER_ANIMALS }
-            case 3:
-                return { type: searchTypes.SHELTERS }
-            default:
-                return { type: searchTypes.DOGS }
-        }
-      }
+    switch (searchChoice) {
+        case 0:
+            return { type: searchTypes.DOGS }
+        case 1:
+            return { type: searchTypes.CATS }
+        case 2:
+            return { type: searchTypes.OTHER_ANIMALS }
+        case 3:
+            return { type: searchTypes.SHELTERS }
+        default:
+            return { type: searchTypes.DOGS }
+    }
+}
