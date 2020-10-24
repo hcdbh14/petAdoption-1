7425bac0ca9439793ee165ffa5f78dca000a2477
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-10-24 11:17:15 +0300

    small fix

--- src/components/layout/header/HeaderImage.js
+++ src/components/layout/header/HeaderImage.js
@@ -18,14 +18,22 @@ const HeaderImage = () => {
 
             <div className="search-container" style={searchState.searchToggle == true ? { width: "50vw", height: "200px" } : { width: "4vw", height: "60x" }} >
                 <button className="search-button" onClick={searchState.searchToggle == true ? () => { handleCategorySwitch(5) } : () => { handleCategorySwitch(4) }} />
-                <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(0)}>כלבים</button>
-                <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(1)}>חתולים</button>
-                <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(2)}>חיות אחרות</button>
-                <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(3)}>עמותות אימוץ</button>
-                {searchState.searchChoice == 0 ? <h3>dogs</h3> : null}
-                {searchState.searchChoice == 1 ? <h3>cats</h3> : null}
-                {searchState.searchChoice == 2 ? <h3>other animals</h3> : null}
-                {searchState.searchChoice == 3 ? <h3>shelters</h3> : null}
+                {searchState.searchToggle == true ?
+                    <div>
+                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(0)}>כלבים</button>
+                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(1)}>חתולים</button>
+                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(2)}>חיות אחרות</button>
+                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(3)}>עמותות אימוץ</button>
+                    </div>
+                    : null}
+                {searchState.searchToggle == true ?
+                    <div>
+                        {searchState.searchChoice == 0 ? <h3>dogs</h3> : null}
+                        {searchState.searchChoice == 1 ? <h3>cats</h3> : null}
+                        {searchState.searchChoice == 2 ? <h3>other animals</h3> : null}
+                        {searchState.searchChoice == 3 ? <h3>shelters</h3> : null}
+                    </div>
+                    : null}
             </div>
         </div>
     )
