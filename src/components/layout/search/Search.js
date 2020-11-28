314dd883b7bc1b375a271bcf981a5656139c7dbf
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-11-28 09:59:52 +0200

    added pet cards

--- src/components/layout/search/Search.js
+++ src/components/layout/search/Search.js
@@ -81,6 +81,96 @@ const Search = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div class="wrap-collabsible">
+                        <input id="collapsible5" class="toggle" type="checkbox" />
+                        <label for="collapsible5" class="lbl-toggle">גיל</label>
+                        <div class="collapsible-content">
+                            <div class="content-inner">
+                                <button className="filterButton">צעיר (22)</button>
+                                <button className="filterButton">בוגר (45)</button>
+                                <button className="filterButton">מבוגר (30)</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="pets">
+                    <button className="pet-card">
+                        <img src={require('../../../images/dogy.jpg')} className="pet-preview-image" />
+                        <p className="pet-name-preview">דניאל</p>
+                        <div className="description-preview">
+                            <img src={require('../../../images/heart.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">כלב בינוני בן 5 שנים</p>
+                        </div>
+                        <p className="pet-goodWords-preview">אני אוהב לשחק</p>
+
+                        <div className="description-preview">
+                            <img src={require('../../../images/location.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">מרכז</p>
+                        </div>
+                    </button>
+
+                    <button className="pet-card">
+                        <img src={require('../../../images/dogy.jpg')} className="pet-preview-image" />
+                        <p className="pet-name-preview">דניאל</p>
+                        <div className="description-preview">
+                            <img src={require('../../../images/heart.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">כלב בינוני בן 5 שנים</p>
+                        </div>
+                        <p className="pet-goodWords-preview">אני אוהב לשחק</p>
+
+                        <div className="description-preview">
+                            <img src={require('../../../images/location.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">מרכז</p>
+                        </div>
+                    </button>
+
+                    <button className="pet-card">
+                        <img src={require('../../../images/dogy.jpg')} className="pet-preview-image" />
+                        <p className="pet-name-preview">דניאל</p>
+                        <div className="description-preview">
+                            <img src={require('../../../images/heart.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">כלב בינוני בן 5 שנים</p>
+                        </div>
+                        <p className="pet-goodWords-preview">אני אוהב לשחק</p>
+
+                        <div className="description-preview">
+                            <img src={require('../../../images/location.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">מרכז</p>
+                        </div>
+                    </button>
+
+                    <button className="pet-card">
+                        <img src={require('../../../images/dogy.jpg')} className="pet-preview-image" />
+                        <p className="pet-name-preview">דניאל</p>
+                        <div className="description-preview">
+                            <img src={require('../../../images/heart.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">כלב בינוני בן 5 שנים</p>
+                        </div>
+                        <p className="pet-goodWords-preview">אני אוהב לשחק</p>
+
+                        <div className="description-preview">
+                            <img src={require('../../../images/location.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">מרכז</p>
+                        </div>
+                    </button>
+
+
+                    <button className="pet-card">
+                        <img src={require('../../../images/dogy.jpg')} className="pet-preview-image" />
+                        <p className="pet-name-preview">דניאל</p>
+                        <div className="description-preview">
+                            <img src={require('../../../images/heart.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">כלב בינוני בן 5 שנים</p>
+                        </div>
+                        <p className="pet-goodWords-preview">אני אוהב לשחק</p>
+
+                        <div className="description-preview">
+                            <img src={require('../../../images/location.png')} className="pet-heart-preview" />
+                            <p className="pet-desc-preview">מרכז</p>
+                        </div>
+                    </button>
                 </div>
             </div>
         </div>
