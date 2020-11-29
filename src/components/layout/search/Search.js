2ca5f3c885693d8e5615aeff7882e6553f93398f
kenny kurochkin <kennnkuro15@gmail.com>
2020-11-29 16:51:41 +0200

    code order part 2

--- src/components/layout/search/Search.js
+++ src/components/layout/search/Search.js
@@ -1,79 +1,13 @@
 import React from 'react'
 import SearchTitle from './SearchTitle';
+import Collapsible from './Collapsible';
 
 const Search = () => {
     return (
         <div className="search">
             <SearchTitle />
-            <div className="search__box">
-                <div className="search__box__advanced">
-                    <p className="search__box__advanced__title">חיפוש מתקדם</p>
-
-                    <div class="wrap-collabsible">
-                        <input id="collapsible1" class="toggle" type="checkbox" />
-                        <label for="collapsible1" class="lbl-toggle">לפי שם</label>
-                        <div class="collapsible-content">
-                            <div class="content-inner">
-                                <div class="wrap">
-                                    <div class="name-search">
-                                        <input type="text" class="searchTerm" placeholder="ספלאנק" />
-                                        <img src={require('../../../images/search-icon.png')} alt="Logo" className="searchButton" />
-
-                                    </div>
-                                    <button class="update-button">חיפוש</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="wrap-collabsible">
-                        <input id="collapsible2" class="toggle" type="checkbox" />
-                        <label for="collapsible2" class="lbl-toggle">סוג חייה</label>
-                        <div class="collapsible-content">
-                            <div class="content-inner">
-                                <button className="filterButton">כולם (100)</button>
-                                <button className="filterButton">כלב (25)</button>
-                                <button className="filterButton">חתול (63)</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="wrap-collabsible">
-                        <input id="collapsible3" class="toggle" type="checkbox" />
-                        <label for="collapsible3" class="lbl-toggle">אזור</label>
-                        <div class="collapsible-content">
-                            <div class="content-inner">
-                                <button className="filterButton">צפון (33)</button>
-                                <button className="filterButton">מרכז (78)</button>
-                                <button className="filterButton">דרום (11)</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="wrap-collabsible">
-                        <input id="collapsible4" class="toggle" type="checkbox" />
-                        <label for="collapsible4" class="lbl-toggle">מין</label>
-                        <div class="collapsible-content">
-                            <div class="content-inner">
-                                <button className="filterButton">זכר (53)</button>
-                                <button className="filterButton">נקבה (54)</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="wrap-collabsible">
-                        <input id="collapsible5" class="toggle" type="checkbox" />
-                        <label for="collapsible5" class="lbl-toggle">גיל</label>
-                        <div class="collapsible-content">
-                            <div class="content-inner">
-                                <button className="filterButton">צעיר (22)</button>
-                                <button className="filterButton">בוגר (45)</button>
-                                <button className="filterButton">מבוגר (30)</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="search__options">
+                <Collapsible />
 
                 <div className="pets">
                     <button className="pet-card">
