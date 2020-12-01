6a54b54cbc3804b0be027585891fcc253bbd88ea
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-12-01 22:03:25 +0200

    About beginning

--- src/components/screens/AboutPage.js
+++ src/components/screens/AboutPage.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const AboutPage = () => {
     return (
-
         <div className="about">
             <h1 className="about__blueTitle">אז מי אנחנו?</h1>
+            <p className="about__paragraph">
+                אנחנו מאמינים שכלבים וחתולים הם הרבה מעבר לחיות <br />
+            מחמד - הם חלק בלתי נפרד מהמשפחה, וכל יום במחיצתם <br />
+            הוא כיף גדול וחוויה חדשה שמשדרגת את איכות החיים <br />
+            וממלאת אותה בשמחה, חום ואהבה. <br />
+            האהבה הזאת, היא שגורמת לנו לרצות, בכל יום מחדש, <br />
+            לעזור להפוך את החיים שלהם לטובים ובריאים יותר.
+            </p>
+            <h1 className="about__orangeTitle">מה אתם צריכים לעשות?</h1>
+            <p className="about__paragraph">
+                לפתוח את הלב ולאמץ חבר חדש <br />
+                שיכניס לכם טונות של אושר לחיים.
+            </p>
         </div >
     )
 }
