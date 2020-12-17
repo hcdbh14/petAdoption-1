d6786ed32c10b8722b03251d11c91c31c6d7f7cf
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-12-17 10:06:24 +0200

    started container work

--- src/components/layout/search/Search.js
+++ src/components/layout/search/Search.js
@@ -1,22 +1,10 @@
-import React from 'react'
-import PetCard from './PetCard';
-import SearchTitle from './SearchTitle';
-import Collapsible from './Collapsible';
+import React from 'react';
+import Title from './Title';
 
 const Search = () => {
     return (
         <div className="search">
-            <SearchTitle />
-            <div className="search__section"> 
-                <Collapsible />
-                <div className="pets">
-                    <PetCard />
-                    <PetCard />
-                    <PetCard />
-                    <PetCard />
-                    <PetCard />
-                </div>
-            </div>
+            <Title />
         </div>
     )
 }
