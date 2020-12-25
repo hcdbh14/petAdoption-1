66f1cebc7921e536e09d3d446535d922e4b30a11
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-12-25 12:03:27 +0200

    work

--- src/components/layout/header/HeaderImage.js
+++ src/components/layout/header/HeaderImage.js
@@ -1,43 +1,16 @@
 import React, { useState } from 'react';
-import { switchSearchCategory } from '../../../store/headerSearch/action';
-import { useSelector, useDispatch } from 'react-redux';
+
 
 const HeaderImage = () => {
 
-    const searchState = useSelector(state => state.headerSearchReducer);
-    const dispatch = useDispatch();
-
-    const handleCategorySwitch = (userChoice) => {
-        dispatch(switchSearchCategory(userChoice));
-    }
-
     return (
         <div className="main__shoutout">
-
-
-            {/*  temporary search container - might change or removed in near future */}
-            {/* 
-            <input type="image" className="login-button" src={require('../../../images/login-icon.png')} alt="login-button" />
-
-            <div className="search-container" style={searchState.searchToggle == true ? { width: "50vw", height: "200px" } : { width: "4vw", height: "60x" }} >
-                <button className="search-button" onClick={searchState.searchToggle == true ? () => { handleCategorySwitch(5) } : () => { handleCategorySwitch(4) }} />
-                {searchState.searchToggle == true ?
-                    <div>
-                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(0)}>כלבים</button>
-                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(1)}>חתולים</button>
-                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(2)}>חיות אחרות</button>
-                        <button type="button" className="search-category-button" onClick={() => handleCategorySwitch(3)}>עמותות אימוץ</button>
-                    </div>
-                    : null}
-                {searchState.searchToggle == true ?
-                    <div>
-                        {searchState.searchChoice == 0 ? <h3>dogs</h3> : null}
-                        {searchState.searchChoice == 1 ? <h3>cats</h3> : null}
-                        {searchState.searchChoice == 2 ? <h3>other animals</h3> : null}
-                        {searchState.searchChoice == 3 ? <h3>shelters</h3> : null}
-                    </div>
-                    : null}
-            </div> */}
+                 {/* <img src={require('../../../images/dog-model.svg')} alt="pet with owner" className="main__shoutout" /> */}
+                 <h2 className="main__shoutout__title">עמותות כלבים וחתולים הצטרפו אלינו! </h2>
+              
+                 <p className="main__shoutout__text">אנחנו נציגי העמותות קוראים לכם, להצטרף אלינו לאימוץ.
+                     כדי שנוכל לעזור גם לכלבים וחתולים למצוא בית.
+                 </p>
         </div>
     )
 }
