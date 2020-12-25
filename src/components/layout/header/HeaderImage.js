3ea423cae620f30f3e6210a06281135f0516dc07
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-12-25 13:54:26 +0200

    finished new header

--- src/components/layout/header/HeaderImage.js
+++ src/components/layout/header/HeaderImage.js
@@ -5,10 +5,10 @@ const HeaderImage = () => {
 
     return (
         <div className="main__shoutout">
-                 {/* <img src={require('../../../images/dog-model.svg')} alt="pet with owner" className="main__shoutout" /> */}
-                 <h2 className="main__shoutout__title">עמותות כלבים וחתולים הצטרפו אלינו! </h2>
-              
-                 <p className="main__shoutout__text">אנחנו נציגי העמותות קוראים לכם, להצטרף אלינו לאימוץ.
+
+                 <h2 className="main__shoutout__title">עמותות כלבים וחתולים <br /> הצטרפו אלינו! </h2>
+    
+                 <p className="main__shoutout__text">אנחנו נציגי העמותות קוראים לכם, להצטרף אלינו לאימוץ.<br />
                      כדי שנוכל לעזור גם לכלבים וחתולים למצוא בית.
                  </p>
         </div>
