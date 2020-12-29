1990a7ce0b17f9243dc3e4fe04c3c9c08c73c3c8
Amit Ashdot <amitashdot@gmail.com>
2020-12-29 15:15:55 +0200

    redesign adoptMe-section

--- src/components/layout/homePage/AdoptMeSection.js
+++ src/components/layout/homePage/AdoptMeSection.js
@@ -1,36 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 const AdoptMeSection = (props) => {
     const { styleColored, style } = props;
 
-    const [isExpand, setIsExpand] = useState(false)
-    const handleClick = () => {
-        setIsExpand(!isExpand)
-    }
     return (
         <>
             <div className="main__title">
                 <h2 style={style}>אמצו אותי גם אני</h2> <h2 className="blue-text" style={styleColored}>רוצה בית</h2>
             </div>
-
-            <img src={require('../../../images/mainImage.png')} alt="pet with owner" className="main__image" />
-
-            <div className="main__text">
-                {/* <p class="accordion">מאות בעלי חיים מחכים לכם לאימוץ בסניפי עמותת תנו לחיות לחיות ברחבי הארץ.</p>
-                {isExpand && <div class="panel">
-                    <p>בחרו את בעל החיים הרצוי, ובעזרת סינונים השונים תוכלו למקד את החיפוש</p>
-                    <p>ולמצוא את החיה המתאימה לכם לאימוץ.</p>
+            <div className="adoptMe-section">
+                <div className="adoptMe-section__topImage">
+                    <img src={require('../../../images/pets-peeking.png')} alt="pet with owner" className="main__image" />
                 </div>
-                }
-                {!isExpand && <button onClick={handleClick}>V</button>} */}
+                <div className="main__text">
+                    <input type="checkbox" id="title" />
+                    <label for="title">מאות בעלי חיים מחכים לכם לאימוץ בסניפי עמותת תנו לחיות לחיות ברחבי הארץ.</label>
 
-
-                <input type="checkbox" id="title" />
-                <label for="title">מאות בעלי חיים מחכים לכם לאימוץ בסניפי עמותת תנו לחיות לחיות ברחבי הארץ.</label>
-
-                <div className="content">
-                    <p>בחרו את בעל החיים הרצוי, ובעזרת סינונים השונים תוכלו למקד את החיפוש</p>
-                    <p>ולמצוא את החיה המתאימה לכם לאימוץ.</p>
+                    <div className="content">
+                        <p>בחרו את בעל החיים הרצוי, ובעזרת סינונים השונים תוכלו למקד את החיפוש</p>
+                        <p>ולמצוא את החיה המתאימה לכם לאימוץ.</p>
+                    </div>
+                </div>
+                <div className="adoptMe-section__work-with-us">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
                 </div>
             </div>
         </>
