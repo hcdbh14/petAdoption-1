23618da961a896563bdbb5c1fe00fdae87d82a63
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-12-29 19:15:34 +0200

    mini search work

--- src/components/layout/homePage/HomePageSearch.js
+++ src/components/layout/homePage/HomePageSearch.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateFilter } from '../../../store/pets/action';
 import { Link } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
+import { SEARCH } from '../../constants/routes';
 
 const HomePageSearch = () => {
 
@@ -10,7 +10,7 @@ const HomePageSearch = () => {
     const petsState = useSelector(state => state.petsReducer);
 
     const petTypeHandleChange = (pickedValue) => {
-    
+
         petsState.searchInputs.petType = pickedValue.target.value
         dispatch(updateFilter())
     }
@@ -31,37 +31,42 @@ const HomePageSearch = () => {
     }
 
     return (
-        <div className="search">
-                        <div class="select">
+        <div className="miniSearch">
+            <h2 className="miniSearch__title">חיפוש חיות</h2>
+            <div>
 
-                            <select value={petsState.searchInputs.petType} onChange={petTypeHandleChange}>
-                                <option value="">סוג חיה</option>
-                                <option value="dog">כלב</option>
-                                <option value="cat">חתול</option>
-                                <option value="other">אחר</option>
-                            </select>
+                <select className="miniSearch__dropdown" value={petsState.searchInputs.petType} onChange={petTypeHandleChange}>
+                    <option value="">סוג חיה</option>
+                    <option value="dog">כלב</option>
+                    <option value="cat">חתול</option>
+                    <option value="other">אחר</option>
+                </select>
 
-                            <select value={petsState.searchInputs.region} onChange={regionHandleChange}>
-                                <option value="">אזור</option>
-                                <option value="north">צפון</option>
-                                <option value="center">מרכז</option>
-                                <option value="south">דרום</option>
-                            </select>
+                <select className="miniSearch__dropdown" value={petsState.searchInputs.region} onChange={regionHandleChange}>
+                    <option value="">אזור</option>
+                    <option value="north">צפון</option>
+                    <option value="center">מרכז</option>
+                    <option value="south">דרום</option>
+                </select>
+                <br />
+                <select className="miniSearch__dropdown" value={petsState.searchInputs.gender} onChange={genderHandleChange}>
+                    <option value="">מין</option>
+                    <option value="male">זכר</option>
+                    <option value="female">נקבה</option>
+                </select>
 
-                            <select value={petsState.searchInputs.gender} onChange={genderHandleChange}>
-                                <option value="">מין</option>
-                                <option value="male">זכר</option>
-                                <option value="female">נקבה</option>
-                            </select>
+                <select className="miniSearch__dropdown" value={petsState.searchInputs.ageGroup} onChange={ageGroupHandleChange}>
+                    <option value="">גיל</option>
+                    <option value="young">צעיר</option>
+                    <option value="adult">בוגר</option>
+                    <option value="elder">מבוגר</option>
+                </select>
+            </div>
 
-                            <select value={petsState.searchInputs.ageGroup} onChange={ageGroupHandleChange}>
-                                <option value="">גיל</option>
-                                <option value="young">צעיר</option>
-                                <option value="adult">בוגר</option>
-                                <option value="elder">מבוגר</option>
-                            </select>
-                        </div>
-                        <Link to={ROUTES.SEARCH} className="navigation__link">חיפוש</Link>
+                <Link to={SEARCH}>
+                    <button className="miniSearch__button" type="button">חיפוש</button>
+                </Link>
+
 
         </div>
     )
