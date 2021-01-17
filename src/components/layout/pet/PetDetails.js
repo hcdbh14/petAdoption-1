e260a2298e4c5027f74f9db701fb8c8383325526
Kenny Kurochkin <kennnkuro15@gmail.com>
2021-01-17 12:29:34 +0200

    work

--- src/components/layout/pet/PetDetails.js
+++ src/components/layout/pet/PetDetails.js
@@ -37,53 +37,115 @@ const PetDetails = () => {
         }
     };
 
+    const buildAgeDesc = () => {
+
+        var formattedAge = detailState.pet.age
+        var maleOrFemale = ""
+        var monthsOrYears = ""
+
+        if (detailState.pet.gender === "זכר") {
+            maleOrFemale = "בן"
+        } else {
+            maleOrFemale = "בת"
+        }
+
+        if (detailState.pet.age >= 1) {
+            monthsOrYears = "שנים"
+
+        } else {
+            monthsOrYears = "חודשים"
+            formattedAge = String(detailState.pet.age).split(".")[1];
+        }
+
+        return maleOrFemale + " " + formattedAge + " " + monthsOrYears;
+    }
+
     useEffect(loadOrSaveState, [])
+
 
     return (
 
-        <div style={{ margin: "500x" }}>
+        <>
             {detailState.pet !== null ?
-                <div>
-                    <h1 style={{ color: "black" }}>{detailState.pet.id}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.name}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.age}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.description}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.gender}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.goodWords}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.phoneNumber}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.poopTrained}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.race}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.region}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.size}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.suitables}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.postDate}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.petType}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.shelter_id}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.vaccinated}</h1>
-                    <h1 style={{ color: "black" }}>{detailState.pet.ageGroup}</h1>
-                    <img className="petCard__image" src={`data:image/png;base64, ${detailState.pet.image}`} alt="תמונת החיה" />
+                <div className="petDetails">
+                    <div className="petDetails__profile">
+                        <p className="petDetails__title">{detailState.pet.petType}, {detailState.pet.name}</p>
+                        <p className="petDetails__detail">
+                            <img className="petDetails__icon" src={require('../../../images/paw-profile.svg')} alt="אייקון של רגל בעל חיים" />
+                            {detailState.pet.race}
+                        </p>
+                        <p className="petDetails__detail">
+                            <img className="petDetails__icon" src={require('../../../images/cake-profile.svg')} alt="אייקון של עוגה" />
+                            {buildAgeDesc()}
+                        </p>
+                        <p className="petDetails__detail">
+                            <img className="petDetails__icon" src={require('../../../images/location-profile.svg')} style={{ width: '30px', height: '37px' }} alt="אייקון של מיקום" />
+                              אזור:   {detailState.pet.region}
+                        </p>
 
-                    {detailState.imagesError !== "" ?
-                        <h1>error</h1>
-                        : (detailState.imagesLoading ?
-                            <h1>images loading</h1>
-                            :
-                            <h1>images ready!</h1>
-                        )}
+                        <p className="petDetails__detail">
+                            <img className="petDetails__icon" src={require('../../../images/phone-profile.svg')} style={{ width: '30px', height: '30px' }} alt="אייקון של טלפון" />
+                            טלפון: {detailState.pet.phoneNumber}
+                        </p>
 
-                    {detailState.shelterError !== "" ?
-                        <h1>error</h1>
-                        : (detailState.shelterLoading ?
-                            <h1>shelter loading</h1>
-                            :
-                            <h1>shelter ready!</h1>
-                        )}
+                        <p className="petDetails__title">קצת עלי</p>
+                        <p className="petDetails__detail">
+                            {detailState.pet.description}
+                        </p>
+                        <p className="petDetails__title">מתאים ל</p>
+                        <p className="petDetails__detail">
+                            {detailState.pet.suitables}
+                        </p>
+
+                        <p className="petDetails__iAm">
+                            <img className="petDetails__icon" src={require('../../../images/checked.svg')} style={{ width: '30px', height: '30px' }} alt="מסומן" />
+                            טלפון: {detailState.pet.phoneNumber}
+                        </p>
+
+
+                        <p className="petDetails__iAm">
+                            <img className="petDetails__icon" src={require('../../../images/notChecked.svg')} style={{ width: '30px', height: '30px' }} alt="לא מסומן" />
+                            טלפון: {detailState.pet.phoneNumber}
+                        </p>
+                        <h1 style={{ color: "black" }}>{detailState.pet.goodWords}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.phoneNumber}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.poopTrained}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.race}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.region}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.size}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.suitables}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.postDate}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.petType}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.shelter_id}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.vaccinated}</h1>
+                        <h1 style={{ color: "black" }}>{detailState.pet.ageGroup}</h1>
+                        <img className="petCard__image" src={`data:image/png;base64, ${detailState.pet.image}`} alt="תמונת החיה" />
+
+                        {detailState.imagesError !== "" ?
+                            <h1>error</h1>
+                            : (detailState.imagesLoading ?
+                                <h1>images loading</h1>
+                                :
+                                <h1>images ready!</h1>
+                            )}
+
+                        {detailState.shelterError !== "" ?
+                            <h1>error</h1>
+                            : (detailState.shelterLoading ?
+                                <h1>shelter loading</h1>
+                                :
+                                <h1>shelter ready!</h1>
+                            )}
+                    </div>
+
+                    <div className="petDetails__imageSection">
+                        <h1>test</h1>
+                    </div>
                 </div>
                 :
-                <div></div>
+                <div />
             }
-        </div >
-
+        </>
     )
 }
 
